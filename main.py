f8f938712f847ad96101ab15a0c8ea6e46b0937c
scagle <sjcagle1@gmail.com>
2019-04-21 01:17:32 -0700

    added adc code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from pdb import set_trace # for debugging
 import mraa               # for GPIO
 
-gpio_1 = mraa.Gpio(23)  # Enable GPIO23
-gpio_1.dir(mraa.DIR_IN) # Set GPIO23 as Input
+print(mraa.getVersion())
 
+try:
+    x = mraa.Aio(0)
+    print(x.read())
+    print("%.f" % x.readFloat())
+except:
+    print("Couldn't find ADC!")
+
+# ir1 = mraa.Gpio(28)       # Enable GPIO28
+# ir1.dir(mraa.DIR_IN)      # Set GPIO28 as Input

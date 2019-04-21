7c25b7daf0ce6671f99af3b3ed2221a3e4028914
Steven Wang <stevenwang312@gmail.com>
2019-04-21 02:08:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 count = 0
 while (count < 100):  
-   try:
-    port = mraa.Aio(count);
-    a = port.read();
-    
-    if (a/255 > 90) :
-        pass      #BOTTLE IF FULL
-    elif (a/255 < 50) :
-        pass     #BOTTLE IS HALF FULL
-    elif (a/255 <25) :
-        pass         #YOU MIGHT WANNA CONSIDER REFILL THE MEDICINE
-    elif (a/255 <10) :
-        pass  #TIME TO GO GET MEDICATION
-   except:
-            pass
-       
+    try:
+        port = mraa.Aio(count);
+        a = port.read();
+        
+        if (a/255 > 90) :
+            pass      #BOTTLE IF FULL
+        elif (a/255 < 50) :
+            pass     #BOTTLE IS HALF FULL
+        elif (a/255 <25) :
+            pass         #YOU MIGHT WANNA CONSIDER REFILL THE MEDICINE
+        elif (a/255 <10) :
+            pass  #TIME TO GO GET MEDICATION
+            print("Got a ADC" + count)
+    except:
+        print("Are you sure you have an ADC?")
+        pass

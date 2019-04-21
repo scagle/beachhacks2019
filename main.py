30d0772bbb286192c5dd54342681c0384392003b
Steven Wang <stevenwang312@gmail.com>
2019-04-21 01:39:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 port = mraa.Aio(0);
 a = port.read();
 
-if (a/255 > 90) 
-     #BOTTLE IF FULL
-     pass
-elif (a/255 < 50) :
-    pass
-    #BOTTLE IS HALF FULL
-elif (a/255 <25) :
-    pass
-        #YOU MIGHT WANNA CONSIDER REFILL THE MEDICINE
+if (a/255 > 90) :
 
-else (a/255 <10) 
-pass
-        #TIME TO GO GET MEDICATION
+     pass      #BOTTLE IF FULL
+elif (a/255 < 50) :
+    pass     #BOTTLE IS HALF FULL
+
+elif (a/255 <25) :
+    pass         #YOU MIGHT WANNA CONSIDER REFILL THE MEDICINE
+
+
+elif (a/255 <10) :
+    pass  #TIME TO GO GET MEDICATION
+
+       
